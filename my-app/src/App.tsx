c4554c1d3d86b2e6ac3b916c16ec144dc398bbed
astrahl008 <alexanderstrahl@hotmail.com>
2020-09-16 20:29:15 -0400

    Updated react's default for showwcase

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './showwcase-logo.svg';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Showwcase Developer Challenge
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://showwcase.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Showwcase
         </a>
       </header>
     </div>
